23979bebe70be0a5b8bf52a2aded4b4388008dc9
AEthier-AI <alex.ethie@gmail.com>
2025-09-04 14:16:11 -0400

    1er commit
    Test link Github
    
    Squelette temporaire

---
+++ GRBI_Callseeker.R
@@ -0,0 +1,46 @@
+setwd("c:\\Users\\alexe\\OneDrive\\PROG\\GRBI\\GRBI")
+
+# Chargement des librairies####
+
+
+
+
+#TRI ET SÉLECTION####
+
+
+
+#ANALYSE 1 - Fenêtre d'activité vocale####
+
+
+#ANALYSE 2 - Métriques de performance####
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
