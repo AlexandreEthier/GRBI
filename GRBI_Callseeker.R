87422707317abb40422310db18fe6e4a0b2eb581
AEthier-AI <alex.ethie@gmail.com>
2025-09-07 13:19:18 -0400

    Traitement des données de temps (Heure du début et heure de la vocalise) -> transofrmation en secondes

--- GRBI_Callseeker.R
+++ GRBI_Callseeker.R
@@ -2,9 +2,13 @@ setwd("c:\\Users\\alexe\\OneDrive\\PROG\\GRBI\\GRBI")
 
 # Chargement des librairies####
 
+install.packages("lubridate")
+
+library(lubridate)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+
 
 # Chargement du jeu de données####
 
@@ -29,10 +33,16 @@ str(act)
 str(occu)
 
 
-
 #TRI ET SÉLECTION####
 # On veut tout d'abord séparer les deux classes en AM et PM
 
+act_AM <- dplyr::filter(act, Periode == "AM")
+act_AM        
+
+
+
+act_PM <- dplyr::filter(act, Periode == "PM")#Activité PM
+act_PM
 
 
 # SNR 
@@ -49,8 +59,44 @@ str(occu)
 
 #Analyse préliminaire pour voir le patron journalier
 
-sun_class <- seq(from = 1, to = 4000)
+H_duree_AM <- act_AM %>% 
+  group_by(Heure_debut, duree) %>% #Groupe l'heure de début (hh:mm) avec la durée de l'enregistrement (en s) 
+  summarise(Heure_debut,duree, start_time_cor2018)
+H_duree_AM
 
+# Transformer temps(hh:mm) en secondes, additionner la colonne «start_time_cor2018)  
+
+hms_AM <- lubridate::hms(H_duree_AM$Heure_debut) # Transformation de l'heure de début en HMS (Heure, minutes, secondes)
+hms_AM
+
+sec_AM <- period_to_seconds(hms_AM) # Transformation du HMS en secondes
+sec_AM
+
+min(sec_AM) #12660
+max(sec_AM) #18000
+  
+# Jointure à la table H_duree_AM
+
+H_duree_AM <- cbind(H_duree_AM, Heure_deb_sec = sec_AM)
+
+#Addition des deux colonnes sec_AM et start_time_cor2018 pour avoir le temps (en secondes)
+
+debut_start <- H_duree_AM[c(3,4)] #Sélection des colonnes qui sont intéressantes pour faire rowSums
+
+as.numeric(debut_start$start_time_cor2018)
+as.numeric(debut_start$Heure_deb_sec)
+
+class(debut_start$Heure_deb_sec)
+class(debut_start$start_time_cor2018)
+
+sec_voc_AM <- rowSums(debut_start) #Temps(secondes) des vocalises
+
+
+#On veut créer des "classes" de temps en fct. de l'heure du lever/coucher de soleil (qui correspondra au temps 0)
+
+sun_vec <-  #Avec le min et max des secondes, positionner un axe pour lequel le lever (AM) et coucher (PM) correspond à 0
+  
+  
 
 
 
